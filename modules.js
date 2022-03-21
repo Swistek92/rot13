50b1ffb8d2606247741009e1a5faeee6d24c9f62
swistek <swistekxd@gmail.com>
2022-03-21 23:47:40 +0100

    variables types fix, clean code add pop-up output

--- modules.js
+++ modules.js
@@ -1,11 +1,17 @@
-import casar13 from './caser13.js';
+import caesar13 from './caesar13.js';
 const name = document.getElementById('name');
 const out = document.getElementById('out');
 const form = document.getElementById('form');
-console.log(form);
+const box = document.getElementById('box');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  let output = casar13(name.value);
+  let output = caesar13(name.value);
   out.innerHTML = output;
+
+  if (name.value === '') {
+    box.style.display = 'none';
+  } else {
+    box.style.display = 'block';
+  }
 });
