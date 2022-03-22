bc622d561c67f5db03670a77a13e8cd65e2bfca3
swistek <swistekxd@gmail.com>
2022-03-22 10:32:32 +0100

    add language selector

---
+++ db/language.js
@@ -0,0 +1,37 @@
+const language = {
+  english: {
+    title: '“Caesar cipher”',
+    description:
+      'Caesar Cipher A shift cipher is a simple and popular data encryption technique. It is based on a shift of letters in the Alphabet currently uses a shift of 13 letters - ROT13',
+    warning: 'Attention! encrypting only encrypted Latin alphabet characters!',
+
+    titleEnc: 'Encryption',
+    labelEnc: 'Enter data',
+    btnEnc: 'Encryption',
+    outEnc: 'Encrypted data:',
+
+    titleDec: 'Decryption',
+    labelDec: 'Enter encrypted data:',
+    btnDec: 'Decryption',
+    outDec: 'Decrypted data:',
+  },
+  polish: {
+    title: '“Szyfr Cezara”',
+    description:
+      'Szyfr Cezara szyfr przesuwający to prosta i popularna technika szyfrowania danych. Opiera się o przesunięcie liter w Alfabecie obecnie używa się przesunięcia o 13 liter - ROT13',
+    warning:
+      'Uwaga! zaszyfrowaniu zostają poddane tylko znaki alfabetu łacińskiego!',
+
+    titleEnc: 'Szyfrowanie',
+    labelEnc: 'Wprowadz dane',
+    btnEnc: 'Szyfrowanie',
+    outEnc: 'Zaszyfrowane Dane:',
+
+    titleDec: 'Deszyfrowanie',
+    labelDec: 'Wprowadz zaszyfrowane dane:',
+    btnDec: 'Deszyfrowanie',
+    outDec: 'Oszyfrowane dane:',
+  },
+};
+
+export default language;
--- modules.js
+++ modules.js
@@ -1,6 +1,6 @@
-import Encryption from './Encryption.js';
-import Decryption from './Decryption.js';
-
+import Encryption from './logic/Encryption.js';
+import Decryption from './logic/Decryption.js';
+import language from './db/language.js';
 const nameEncrypt = document.getElementById('nameEncrypt');
 const outEncrypt = document.getElementById('outEncrypt');
 const formEncrypt = document.getElementById('formEncrypt');
@@ -10,6 +10,44 @@ const nameDecrpt = document.getElementById('nameDecrpt');
 const outDecrpt = document.getElementById('outDecrpt');
 const formDecrpt = document.getElementById('formDecrpt');
 const boxDecrpt = document.getElementById('boxDecrpt');
+
+const link = document.querySelectorAll('a');
+const langE = document.querySelector('.langWrap');
+const title = document.querySelector('.title');
+const description = document.querySelector('.description');
+const warning = document.querySelector('.warning');
+
+const labelEncrypt = document.querySelector('.labelEncrypt');
+const titleEnrypt = document.querySelector('.titleEnrypt');
+const btnEncrypt = document.querySelector('.btnEncrypt');
+const labelOutEncrypt = document.querySelector('.labelOutEncrypt');
+
+const titleDecrypt = document.querySelector('.titleDecrypt');
+const labelDecrypt = document.querySelector('.labelDecrypt');
+const btnDecrypt = document.querySelector('.btnDecrypt');
+const labelOutDecrypt = document.querySelector('.labelOutDecrypt');
+
+link.forEach((el) => {
+  el.addEventListener('click', () => {
+    langE.querySelector('.active').classList.remove('active');
+    el.classList.add('active');
+
+    const attr = el.getAttribute('language');
+    title.textContent = language[attr].title;
+    description.textContent = language[attr].description;
+    warning.textContent = language[attr].warning;
+
+    titleEnrypt.textContent = language[attr].titleEnc;
+    labelEncrypt.textContent = language[attr].labelEnc;
+    btnEncrypt.textContent = language[attr].btnEnc;
+    labelOutEncrypt.textContent = language[attr].outEnc;
+
+    titleDecrypt.textContent = language[attr].titleDec;
+    labelDecrypt.textContent = language[attr].labelDec;
+    btnDecrypt.textContent = language[attr].btnDec;
+    labelOutDecrypt.textContent = language[attr].outDec;
+  });
+});
 
 formEncrypt.addEventListener('submit', (e) => {
   e.preventDefault();
